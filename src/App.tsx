9e93fb266f16a6c29eff06e5b420ebf9fb194ed2
ThaiLuu92 <matienjp92@gmail.com>
2023-10-05 12:45:10 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  type Todo = {
+    id: number;
+    text: string;
+    date: string
+  };
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
+  
+  const addTodo = () => {
+    if (newTodo.trim() === '') {
+      return;
+    }
+
+    const newTodoItem: Todo = {
+      id: Date.now(),
+      text: newTodo,
+      date: new Date().toLocaleDateString()
+    };
+
+    setTodos([...todos, newTodoItem]);
+    setNewTodo('');
+
+  };
+
+
+  const deleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+ 
+  const countCharacters = (text: string) => {
+    if (text.length <= 200) {
+      return 200 - text.length;
+ 
+   } else {
+    return 0; 
+   }
+    
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='App-title'><h3>Todo List nha Béc Cọp Không ăn được nha</h3><i>Đổi màu nền ở đây</i></div>
+      <div id='seacrch-bar'><input type="text" placeholder='Search here...' /></div>
+      <div className='tasks-area'>
+      {todos.map((todo) =>(
+        <div className='task-add task'  key={todo.id}>
+          <textarea name="" id="" cols={10} rows={7} 
+            value={todo.text}
+            
+/>
+          <div className='task-add-footer'>
+            <div className='task-date'>{todo.date}</div>
+            <button className='btn' id='btn-del' onClick={() => deleteTodo(todo.id)}>DELETE</button>
+          </div>
+        </div>
+      ))}
+        <div className='task-add'>
+          <textarea name="" id="" cols={10} rows={7} defaultValue={""} placeholder='Nhập nội dung công việc ...' 
+            value={newTodo}
+            maxLength={200}
+            onChange={(e) => setNewTodo(e.target.value)}/>
+          <div className='task-add-footer'>
+            <div className='task-date'>Ký tự còn lại: {countCharacters(newTodo)}</div>
+            <button className='btn' id='btn-add' onClick={addTodo}>ADD</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
