9d1d284405af76c08246b3c224978e91870dabd1
ThaiLuu92 <matienjp92@gmail.com>
2023-10-05 19:00:28 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,17 @@ const App: React.FC = () => {
 
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
-  
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  const appStyles = {
+    backgroundColor: isDarkMode ? '#333' : 'white',
+    color: isDarkMode ? 'white' : 'black',
+  };
+
   const addTodo = () => {
     if (newTodo.trim() === '') {
       return;
@@ -33,47 +43,57 @@ const App: React.FC = () => {
     setTodos(updatedTodos);
   };
 
- 
+
   const countCharacters = (text: string) => {
     if (text.length <= 200) {
       return 200 - text.length;
- 
-   } else {
-    return 0; 
-   }
-    
+
+    } else {
+      return 0;
+    }
+
   }
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.text.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
 
 
 
   return (
-    <div className="App">
-      <div id='App-title'><h3>Todo List nha Béc Cọp Không ăn được nha</h3><i>Đổi màu nền ở đây</i></div>
-      <div id='seacrch-bar'><input type="text" placeholder='Search here...' /></div>
+    <div className="App" style={appStyles}>
+      <div id='App-title'><h3>Todo List nha Béc Cọp Không ăn được nha</h3><i style={{ color: isDarkMode ? 'white' : 'black' }}><button onClick={toggleDarkMode} id='btn-dark'>Toggle Dark Mode</button></i></div>
+      <div id='seacrch-bar'><input type="text" placeholder='Search here...' value={searchKeyword}
+        onChange={handleSearchInputChange} /></div>
       <div className='tasks-area'>
-      {todos.map((todo) =>(
-        <div className='task-add task'  key={todo.id}>
-          <textarea name="" id="" cols={10} rows={7} 
-            value={todo.text}
-            
-/>
-          <div className='task-add-footer'>
-            <div className='task-date'>{todo.date}</div>
-            <button className='btn' id='btn-del' onClick={() => deleteTodo(todo.id)}>DELETE</button>
+        {filteredTodos.map((todo) => (
+          <div className='task-add task' key={todo.id}>
+            <textarea name="" id="" cols={10} rows={7}
+              value={todo.text}
+
+            />
+            <div className='task-add-footer'>
+              <div className='task-date'>{todo.date}</div>
+              <button className='btn' id='btn-del' onClick={() => deleteTodo(todo.id)}>DELETE</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
         <div className='task-add'>
-          <textarea name="" id="" cols={10} rows={7} defaultValue={""} placeholder='Nhập nội dung công việc ...' 
+          <textarea name="" id="" cols={10} rows={7} defaultValue={""} placeholder='Nhập nội dung công việc ...'
             value={newTodo}
             maxLength={200}
-            onChange={(e) => setNewTodo(e.target.value)}/>
+            onChange={(e) => setNewTodo(e.target.value)} />
           <div className='task-add-footer'>
             <div className='task-date'>Ký tự còn lại: {countCharacters(newTodo)}</div>
             <button className='btn' id='btn-add' onClick={addTodo}>ADD</button>
           </div>
         </div>
       </div>
+
     </div>
   );
 }
